2bb1fcf1b13fd75ce83dc6f01f96a2491ec055e4
gmonnier <gregory.monnier@360learning.com>
2023-04-28 22:11:03 +0200

    first apple tutorial

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -6,19 +6,74 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text(" reload please !!")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .foregroundColor(.blue)
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+
+            VStack(alignment: .leading) {
+                
+                Text("Turtle Rock")
+                  .font(.title)
+
+                HStack {
+                  Text("Joshua Tree National Park")
+                  Spacer()
+                  Text("California")
+
+                }.font(.subheadline)
+                    .foregroundColor(.secondary)
+                
+                Divider()
+
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+              }
+              .padding()
+            
+            Spacer()
         }
-        .padding()
+
+    }
+}
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 5)
+            }.shadow(radius: 10)
+    }
+}
+
+
+struct MapView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
+    
+    var body: some View {
+        VStack {
+            Map(coordinateRegion: $region)
+            Text("Center is \(region.center.latitude)")
+        }
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
     }
 }
 
