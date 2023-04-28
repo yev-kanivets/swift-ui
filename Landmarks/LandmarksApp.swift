113e3dada8d4dd9e53017fd635100366fafb0ce3
Yev Kanivets <yevhenii.kanivets@gmail.com>
2023-04-28 15:51:06 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Yev Kanivets on 28/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
